c64b52746df603935f1bc6baab1c1cba06fa3c59
mike <mike@localhost>
2013-08-25 12:19:35 -0400

    Decoupled NuGet file generation from repository versioning (Now you just run a script and specify assembly version and release notes). Changed NuGet spec file to pull values from assembly where possible.

--- src/SAML2/Properties/AssemblyInfo.cs
+++ src/SAML2/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SAML2")]
 [assembly: AssemblyDescription("A .NET implementation of the SAML 2.0 specification for SP integrations.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Hallock")]
 [assembly: AssemblyProduct("SAML2")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.4.*")]
-[assembly: AssemblyFileVersion("2.2.4.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
