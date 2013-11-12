6237543ad76d7386d890192bfa854e3a9795b884
mike <mike@localhost>
2013-11-12 00:51:23 -0500

    Work item 1 merge formatting fix.

---
+++ src/SAML2/Identity/Saml20PrincipalCache.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Principal;
+using System.Web;
+
+namespace SAML2.Identity
+{
+    /// <summary>
+    /// The Principal cache for SAML2.
+    /// </summary>
+    internal class Saml20PrincipalCache
+    {
+        /// <summary>
+        /// Adds the principal.
+        /// </summary>
+        /// <param name="principal">The principal.</param>
+        internal static void AddPrincipal(IPrincipal principal)
+        {
+            HttpContext.Current.Session[typeof(Saml20Identity).FullName] = principal;
+        }
+
+        /// <summary>
+        /// Clears this instance.
+        /// </summary>
+        internal static void Clear()
+        {
+            HttpContext.Current.Session.Remove(typeof(Saml20Identity).FullName);
+        }
+
+        /// <summary>
+        /// Gets the principal.
+        /// </summary>
+        /// <returns>The <see cref="IPrincipal"/>.</returns>
+        internal static IPrincipal GetPrincipal()
+        {
+            return HttpContext.Current.Session[typeof(Saml20Identity).FullName] as GenericPrincipal;
+        }
+    }
+}
