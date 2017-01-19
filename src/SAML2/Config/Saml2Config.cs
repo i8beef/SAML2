2f2791f3ffffeced52f0a63b8b5fd8675893268c
Maciej Koperski <maciej@fireflylearning.com>
2017-01-19 12:37:49 +0000

    Cosmetic refactoring changes

--- src/SAML2/Config/Saml2Config.cs
+++ src/SAML2/Config/Saml2Config.cs
@@ -12,6 +12,7 @@ namespace SAML2.Config
         /// The configuration
         /// </summary>
         private static Saml2Section _config;
+        private const string PATH = "~/SAML2.config";
 
         /// <summary>
         /// Gets the config.
@@ -21,17 +22,7 @@ namespace SAML2.Config
         {
             if (_config == null)
             {
-                ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
-                string physicalWebAppPath = System.Web.Hosting.HostingEnvironment.MapPath("~/SAML2.config");
-
-                if (System.IO.File.Exists(physicalWebAppPath))
-                {
-                    fileMap.ExeConfigFilename = physicalWebAppPath;
-                    Configuration configFile = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
-                    _config = configFile.GetSection(Saml2Section.Name) as Saml2Section;
-                }
-
-                if (_config == null)
+                if (!TryLoadConfig(PATH, out _config))
                 {
                     throw new ConfigurationErrorsException(string.Format("Configuration section \"{0}\" not found", typeof(Saml2Section).Name));
                 }
@@ -47,24 +38,29 @@ namespace SAML2.Config
         /// </summary>
         public static void Refresh()
         {
-            _config = null;
+            if (!TryLoadConfig(PATH, out _config))
+            {
+                throw new ConfigurationErrorsException(string.Format("Configuration section \"{0}\" not found", typeof(Saml2Section).Name));
+            }
+
+            _config.IdentityProviders.Refresh();
+        }
+
+        private static bool TryLoadConfig(string path, out Saml2Section config)
+        {
+            config = null;
             ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
-            string physicalWebAppPath = System.Web.Hosting.HostingEnvironment.MapPath("~/SAML2.config");
+            string physicalWebAppPath = System.Web.Hosting.HostingEnvironment.MapPath(path);
 
             if (System.IO.File.Exists(physicalWebAppPath))
             {
                 fileMap.ExeConfigFilename = physicalWebAppPath;
                 Configuration configFile = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
                 ConfigurationManager.RefreshSection(Saml2Section.Name);
-                _config = configFile.GetSection(Saml2Section.Name) as Saml2Section;
+                config = configFile.GetSection(Saml2Section.Name) as Saml2Section;
+                return true;
             }
-
-            if (_config == null)
-            {
-                throw new ConfigurationErrorsException(string.Format("Configuration section \"{0}\" not found", typeof(Saml2Section).Name));
-            }
-
-            _config.IdentityProviders.Refresh();
+            return false;
         }
     }
 }
