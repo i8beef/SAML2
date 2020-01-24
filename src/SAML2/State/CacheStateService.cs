a7e80d3fba4f23afeaae1b4f126a4547021caa6c
Michael Hallock <mike@hallock.net>
2020-01-24 16:01:28 -0500

    38 Adds SameSite disable as per Microsoft's recommendations

--- src/SAML2/State/CacheStateService.cs
+++ src/SAML2/State/CacheStateService.cs
@@ -125,6 +125,41 @@ namespace SAML2.State
             return string.Format("{0}-{1}", GetCacheKeyPrefix(context), key);
         }
 
+        private static bool DisallowsSameSiteNone(string userAgent)
+        {
+            // Cover all iOS based browsers here. This includes:
+            // - Safari on iOS 12 for iPhone, iPod Touch, iPad
+            // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
+            // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
+            // All of which are broken by SameSite=None, because they use the iOS networking stack
+            if (userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12"))
+            {
+                return true;
+            }
+
+            // Cover Mac OS X based browsers that use the Mac OS networking stack. This includes:
+            // - Safari on Mac OS X.
+            // This does not include:
+            // - Chrome on Mac OS X
+            // Because they do not use the Mac OS networking stack.
+            if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
+                userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+            {
+                return true;
+            }
+
+            // Cover Chrome 50-69, because some versions are broken by SameSite=None, 
+            // and none in this range require it.
+            // Note: this covers some pre-Chromium Edge versions, 
+            // but pre-Chromium Edge does not require SameSite=None.
+            if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
         /// <summary>
         /// Gets the decrypted ticket value.
         /// </summary>
@@ -187,7 +222,10 @@ namespace SAML2.State
                 };
             
             // Hack to support SameSite as SAML POST flow fails this check
-            cookie.Path += "; SameSite=None";
+            if (DisallowsSameSiteNone(context.Request.UserAgent) == false)
+            {
+                cookie.Path += "; SameSite=None";
+            }
 
             context.Response.SetCookie(cookie);
 
